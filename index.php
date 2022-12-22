bdce7a53ef736521e1b268ae3c659069f170e8b1
Anailson Mota <mota.a.santos@gmail.com>
2022-12-22 00:12:57 -0300

    Configura o container da aplicação

--- index.php
+++ index.php
@@ -1,27 +1,18 @@
 <?php
 
+use CoopertalseAPI\API\DC as ApiDC;
 use CoopertalseAPI\API\RenderMiddleware;
 use CoopertalseAPI\Dispositivo\DispositivoAPI;
 use CoopertalseAPI\Motorista\MotoristaAPI;
-use Fig\Http\Message\StatusCodeInterface;
-use Psr\Http\Server\RequestHandlerInterface;
 use Slim\App;
-use Slim\Interfaces\RouteCollectorProxyInterface;
-use Slim\Psr7\Request;
-use Slim\Psr7\Response;
 
 require_once __DIR__ . "/vendor/autoload.php";
 
-$oApp = new App();
+$oApp = new App(ApiDC::getContainer());
 
 $oApp->group("/v1", function() {
-	$this->get("/test2", function($req, $resp) {
-		$resp->withBody(json_encode( [ "dfsdfsdf" ]));
-		return $resp;
-	});
 	$this->group("/motorista", MotoristaAPI::class);
 	$this->group("/dispositivo", DispositivoAPI::class);
 })->add(RenderMiddleware::class);
 
 $oApp->run();
-
